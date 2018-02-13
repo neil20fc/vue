77199ff272c001b8b10d4f24189de0f6fecfaee1
Your Name <you@example.com>
2018-02-13 00:10:03 -0500

    vue

---
+++ client/app/core/components/header/logo.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.logo
+		a.nav-item(href="#")
+			span 
+				strong APP
+				| neil
+</template>
+
+<script>
+
+	export default {
+
+	};
+	
+</script>
+
+<style lang="scss">
+</style>
---
+++ server/public/app/frontend.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{210:function(n,o,e){"use strict";var t=e(299);!function(n){n&&n.__esModule}(t);console.log("Frontend loaded!")},299:function(n,o){},414:function(n,o,e){n.exports=e(210)}},[414]);
